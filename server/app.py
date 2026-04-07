6f5e3ff376dab31bb693dc22450998168bf61e93
cooldeveloper <gamerhustle12@gmail.com>
2026-04-07 22:45:45 +0530

    Update app.py

--- server/app.py
+++ server/app.py
@@ -41,3 +41,7 @@ def step(action: Action):
         "reward": reward,
         "done": done
     }
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("app:app", host="0.0.0.0", port=7860)
